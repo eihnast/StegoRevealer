664dc07fd9c7e94bb0b12251f0454240ba25cb96
Iaroslav Grachev <yaroslav446@gmail.com>
2024-11-13 14:21:04 +0300

    Тестовое обучение модели

--- StegoRevealer.StegoCore.TrainingModule/Program.cs
+++ StegoRevealer.StegoCore.TrainingModule/Program.cs
@@ -4,6 +4,8 @@ using StegoRevealer.StegoCore.AnalysisMethods.RsMethod;
 using StegoRevealer.StegoCore.AnalysisMethods.StatisticalMetrics;
 using StegoRevealer.StegoCore.CommonLib;
 using StegoRevealer.StegoCore.ImageHandlerLib;
+using StegoRevealer_StegoCore_TrainingModule;
+using System.Diagnostics;
 
 namespace StegoRevealer.StegoCore.TrainingModule;
 
@@ -104,93 +106,112 @@ internal class Program
     //    }
     //}
 
-    //static async Task Main(string[] args)
-    //{
-    //    var inputImages = Directory.GetFiles("d:\\Temp\\_training\\_Test600Plus");
-    //    var realImages = new List<string>();
-    //    var analysedData = new List<StegoModelTest2.ModelInput>();
-    //    foreach (var imgPath in inputImages)
-    //    {
-    //        var img = new ImageHandler(imgPath);
+    static async Task Main(string[] args)
+    {
+        var inputImages = Directory.GetFiles("d:\\Temp\\_StegoRevealer\\_training\\_Test600Plus");
+        var realImages = new List<string>();
+        var analysedData = new List<DecisionModel.ModelInput>();
 
-    //        var horizonalChiSqr = new ChiSquareAnalyser(img);
-    //        horizonalChiSqr.Params.TraverseType = TraverseType.Horizontal;
+        var elapsed1920 = new List<long>();
+        var elapsed1280 = new List<long>();
+        var timer = new Stopwatch();
 
-    //        var verticalChiSqr = new ChiSquareAnalyser(img);
-    //        verticalChiSqr.Params.TraverseType = TraverseType.Vertical;
-    //        verticalChiSqr.Params.BlockWidth = 1;
-    //        verticalChiSqr.Params.BlockHeight = img.Height;
+        foreach (var imgPath in inputImages)
+        {
+            timer.Start();
 
-    //        var horizonotalKzha = new KzhaAnalyser(img);
-    //        horizonotalKzha.Params.TraverseType = TraverseType.Horizontal;
+            var img = new ImageHandler(imgPath);
 
-    //        var verticalKzha = new KzhaAnalyser(img);
-    //        verticalKzha.Params.TraverseType = TraverseType.Vertical;
+            var horizonalChiSqr = new ChiSquareAnalyser(img);
+            horizonalChiSqr.Params.TraverseType = TraverseType.Horizontal;
 
-    //        var rs = new RsAnalyser(img);
-    //        var statm = new StatmAnalyser(img);
+            var verticalChiSqr = new ChiSquareAnalyser(img);
+            verticalChiSqr.Params.TraverseType = TraverseType.Vertical;
+            verticalChiSqr.Params.BlockWidth = 1;
+            verticalChiSqr.Params.BlockHeight = img.Height;
+
+            var horizonotalKzha = new KzhaAnalyser(img);
+            horizonotalKzha.Params.TraverseType = TraverseType.Horizontal;
+
+            var verticalKzha = new KzhaAnalyser(img);
+            verticalKzha.Params.TraverseType = TraverseType.Vertical;
+
+            var rs = new RsAnalyser(img);
+            var statm = new StatmAnalyser(img);
 
 
-    //        ChiSquareResult? horizonalChiSqrResult = null;
-    //        KzhaResult? horizonotalKzhaResult = null;
-    //        RsResult? rsResult = null;
-    //        StatmResult? statmResult = null;
-    //        var analysisTasks = new List<Task>
-    //        {
-    //            Task.Run(() => horizonalChiSqrResult = horizonalChiSqr.Analyse()),
-    //            Task.Run(() => horizonotalKzhaResult = horizonotalKzha.Analyse()),
-    //            Task.Run(() => rsResult = rs.Analyse()),
-    //            Task.Run(() => statmResult = statm.Analyse())
-    //        };
+            ChiSquareResult? horizonalChiSqrResult = null;
+            KzhaResult? horizonotalKzhaResult = null;
+            RsResult? rsResult = null;
+            StatmResult? statmResult = null;
+            var analysisTasks = new List<Task>
+            {
+                Task.Run(() => horizonalChiSqrResult = horizonalChiSqr.Analyse()),
+                Task.Run(() => horizonotalKzhaResult = horizonotalKzha.Analyse()),
+                Task.Run(() => rsResult = rs.Analyse()),
+                Task.Run(() => statmResult = statm.Analyse())
+            };
 
-    //        foreach (var task in analysisTasks)
-    //            await task;
+            foreach (var task in analysisTasks)
+                await task;
 
-    //        if (horizonalChiSqrResult is null || horizonotalKzhaResult is null || rsResult is null || statmResult is null)
-    //            continue;
+            if (horizonalChiSqrResult is null || horizonotalKzhaResult is null || rsResult is null || statmResult is null)
+                continue;
 
-    //        var kzhVolume = ContainerVolumeForKzh(img);
-    //        realImages.Add(imgPath);
-    //        analysedData.Add(new StegoModelTest2.ModelInput
-    //        {
-    //            ChiSquareVolume = (float)horizonalChiSqrResult.MessageRelativeVolume,
-    //            RsVolume = (float)rsResult.MessageRelativeVolume,
-    //            KzhaThreshold = (float)horizonotalKzhaResult.Threshold,
-    //            KzhaMessageVolume = horizonotalKzhaResult.MessageBitsVolume / kzhVolume,
-    //            NoiseValue = (float)statmResult.NoiseValue,
-    //            SharpnessValue = (float)statmResult.SharpnessValue,
-    //            BlurValue = (float)statmResult.BlurValue,
-    //            ContrastValue = (float)statmResult.ContrastValue,
-    //            EntropyShennonValue = (float)statmResult.EntropyValues.Shennon,
-    //            EntropyRenyiValue = (float)statmResult.EntropyValues.Renyi
-    //        });
+            var kzhVolume = ContainerVolumeForKzh(img);
+            realImages.Add(imgPath);
+            analysedData.Add(new DecisionModel.ModelInput
+            {
+                ChiSquareVolume = (float)horizonalChiSqrResult.MessageRelativeVolume,
+                RsVolume = (float)rsResult.MessageRelativeVolume,
+                KzhaThreshold = (float)horizonotalKzhaResult.Threshold,
+                KzhaMessageVolume = horizonotalKzhaResult.MessageBitsVolume / kzhVolume,
+                NoiseValue = (float)statmResult.NoiseValue,
+                SharpnessValue = (float)statmResult.SharpnessValue,
+                BlurValue = (float)statmResult.BlurValue,
+                ContrastValue = (float)statmResult.ContrastValue,
+                EntropyShennonValue = (float)statmResult.EntropyValues.Shennon,
+                EntropyRenyiValue = (float)statmResult.EntropyValues.Renyi
+            });
 
-    //        Console.WriteLine($"Завершён анализ для {imgPath}");
-    //    }
+            Console.WriteLine($"Завершён анализ для {imgPath}");
 
-    //    var results = new float[] { 0f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 0f, 1f, 0f, 0f, 0f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 0f, 0f, 0f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 0f, 0f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 0f, 0f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 0f, 1f, 1f, 1f, 0f, 0f, 1f, 1f, 1f, 0f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 0f, 0f, 0f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 0f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 0f, 0f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 0f, 0f, 0f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f };
-    //    //var resultsList = Enumerable.Repeat(0f, 4316).ToList();
-    //    //resultsList.AddRange(Enumerable.Repeat(1f, 4316).ToList());
-    //    //var results = resultsList.ToArray();
+            timer.Stop();
+            if (img.Width == 1920 && img.Height == 1080)
+                elapsed1920.Add(timer.ElapsedMilliseconds);
+            else if (img.Width == 1280 && img.Height == 720)
+                elapsed1280.Add(timer.ElapsedMilliseconds);
+            timer.Reset();
+        }
 
-    //    if (results.Length != analysedData.Count)
-    //    {
-    //        Console.WriteLine($"Размеры не совпадают: results.Count == {results.Length}, analysedData.Count == {analysedData.Count}");
-    //        return;
-    //    }
+        var results = new float[] { 0f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 0f, 1f, 0f, 0f, 0f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 0f, 0f, 0f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 0f, 0f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 0f, 0f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 0f, 1f, 1f, 1f, 0f, 0f, 1f, 1f, 1f, 0f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 0f, 0f, 0f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 0f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 0f, 0f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 0f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 0f, 0f, 0f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f };
+        //var resultsList = Enumerable.Repeat(0f, 4316).ToList();
+        //resultsList.AddRange(Enumerable.Repeat(1f, 4316).ToList());
+        //var results = resultsList.ToArray();
 
-    //    var boolResults = results.Select(r => r == 1f).ToArray();
-    //    for (int i = 0; i < analysedData.Count; i++)
-    //    {
-    //        var hided = StegoModelTest2.Predict(analysedData[i]);
-    //        bool isHidedPredicted = hided.PredictedLabel;
-    //        bool isHidedReal = boolResults[i];
-    //        if (isHidedPredicted != isHidedReal)
-    //            Console.WriteLine($"Не совпало для {i}. Получено: {isHidedPredicted}, Ожидалось: {isHidedReal}. Изображение: {realImages[i]}");
-    //    }
-    //}
+        if (results.Length != analysedData.Count)
+        {
+            Console.WriteLine($"Размеры не совпадают: results.Count == {results.Length}, analysedData.Count == {analysedData.Count}");
+            return;
+        }
 
-    //private static int ContainerVolumeForKzh(ImageHandler img) => (img.Height / 8) * (img.Width / 8);
+        var boolResults = results.Select(r => r == 1f).ToArray();
+        for (int i = 0; i < analysedData.Count; i++)
+        {
+            var hided = DecisionModel.Predict(analysedData[i]);
+            bool isHidedPredicted = hided.PredictedLabel;
+            bool isHidedReal = boolResults[i];
+            if (isHidedPredicted != isHidedReal)
+                Console.WriteLine($"Не совпало для {i}. Получено: {isHidedPredicted}, Ожидалось: {isHidedReal}. Изображение: {realImages[i]}");
+        }
 
-    static void Main(string[] args) { }
+        double averageelapsed1920Elapsed = elapsed1920.Average();
+        double averageelapsed1280Elapsed = elapsed1280.Average();
+        Console.WriteLine($"1920. Среднее время: {averageelapsed1920Elapsed}. Максимальное: {elapsed1920.Max()}. Минимальное: {elapsed1920.Min()}.");
+        Console.WriteLine($"1280. Среднее время: {averageelapsed1280Elapsed}. Максимальное: {elapsed1280.Max()}. Минимальное: {elapsed1280.Min()}.");
+    }
+
+    private static int ContainerVolumeForKzh(ImageHandler img) => (img.Height / 8) * (img.Width / 8);
+
+    // static void Main(string[] args) { }
 }
