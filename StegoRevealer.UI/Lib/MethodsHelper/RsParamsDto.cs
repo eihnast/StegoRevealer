0d59e83d480d5740fc2bfb899b815f30c099c4cb
Iaroslav Grachev <eihnast@gmail.com>
2025-04-22 18:31:41 +0300

    Настройка блоков для RS вынесена на форму

--- StegoRevealer.UI/Lib/MethodsHelper/RsParamsDto.cs
+++ StegoRevealer.UI/Lib/MethodsHelper/RsParamsDto.cs
@@ -1,4 +1,5 @@
 ﻿using StegoRevealer.StegoCore.AnalysisMethods.RsMethod;
+using StegoRevealer.StegoCore.CommonLib;
 using StegoRevealer.StegoCore.CommonLib.ScTypes;
 using StegoRevealer.StegoCore.ImageHandlerLib;
 using StegoRevealer.UI.Lib.ParamsHelpers;
@@ -18,17 +19,25 @@ public class RsParamsDto : IParamsDto<RsParameters>
 
     public int[] FlippingMask { get; set; } = new int[4] { 1, 0, 0, 1 };
 
+    public TraverseType TraverseType { get; set; } = TraverseType.Horizontal;
+
+    public int BlockWidth { get; set; } = 1;
+    public int BlockHeight { get; set; } = 1;
 
     public RsParamsDto() { }
 
     public RsParamsDto(RsParameters parameters)
     {
+        TraverseType = parameters.TraverseType;
         Channels = new();
         foreach (var channel in parameters.Channels)
             Channels.Add(channel);
 
         PixelsGroupLength = parameters.PixelsGroupLength;
         FlippingMask = (int[])parameters.FlippingMask.Clone();
+
+        BlockWidth = parameters.BlockWidth;
+        BlockHeight = parameters.BlockHeight;
     }
 
     /// <inheritdoc/>
@@ -37,11 +46,16 @@ public class RsParamsDto : IParamsDto<RsParameters>
         if (parameters is null)
             return;
 
+        parameters.TraverseType = TraverseType;
+
         parameters.Channels.Clear();
         foreach (var channel in Channels)
             parameters.Channels.Add(channel);
 
         parameters.PixelsGroupLength = PixelsGroupLength;
         parameters.FlippingMask = (int[])FlippingMask.Clone();
+
+        parameters.BlockWidth = BlockWidth;
+        parameters.BlockHeight = BlockHeight;
     }
 }
