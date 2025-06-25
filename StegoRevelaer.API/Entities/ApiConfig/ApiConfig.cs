b80494e702c9b623b4ae70d01dda1bde162a0e03
Iaroslav Grachev <eihnast@gmail.com>
2025-06-26 00:30:34 +0300

    Добавлен конфигуратор API

---
+++ StegoRevelaer.API/Entities/ApiConfig/ApiConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace StegoRevelaer.API.Entities.ApiConfig;
+
+public class ApiConfig
+{
+    public string HttpAddress { get; set; } = "http://localhost:11038";
+    public string HttpsAddress { get; set; } = "https://localhost:11040";
+}
--- StegoRevelaer.API/Program.cs
+++ StegoRevelaer.API/Program.cs
@@ -1,10 +1,15 @@
+using StegoRevelaer.API.Services;
+
 namespace StegoRevelaer.API;
 
 public static class Program
 {
     public static void Main(string[] args)
     {
+        var config = ApiConfigurator.Instance.ApiConfig;
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.WebHost.UseUrls(config.HttpAddress, config.HttpsAddress);
 
         builder.Services.AddControllers();
         builder.Services.AddOpenApi();
